2471edc7474c9758fa08766049cf1ef26ec9e20f
dionyysus <gizemcsk1@gmail.com>
2022-09-05 07:50:59 +0300

    custom cell table view

---
+++ Listeleme İşlemleri/Detaylı TableView Kullanımı/Detaylı TableView Kullanımı/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Detaylı TableView Kullanımı
+//
+//  Created by Gizem Coşkun on 5.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
